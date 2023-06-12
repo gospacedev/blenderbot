7b756208101f7f037bc11358118b2dabd4875b8c
Grantley Cullar <grantcullar@gmail.com>
2023-06-12 21:50:09 +0800

    Add tokenizer

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# blender-chatbot
+# blenderbot
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ mname = "facebook/blenderbot-400M-distill"
 model = BlenderbotForConditionalGeneration.from_pretrained(mname)
 tokenizer = BlenderbotTokenizer.from_pretrained(mname)
 
-chat_history = ("")
+# Store user input and bot responses
+history = ("")
 
 # Create a function to generate a response to a user's input
 def generate_response(user_input):
@@ -13,7 +14,7 @@ def generate_response(user_input):
     inputs = tokenizer(user_input, return_tensors="pt")
 
     # Generate a response
-    reply_ids= model.generate(**inputs)
+    reply_ids= model.generate(**inputs, max_length=60)
 
     # Decode the response
     return tokenizer.batch_decode(reply_ids, skip_special_tokens=True)[0]
@@ -21,9 +22,11 @@ def generate_response(user_input):
 # Start a conversation with the user
 while True:
     user_input = input("User: ")
-    chat_history += user_input
+    history += tokenizer.bos_token + user_input + tokenizer.eos_token + " "
     
-    response = generate_response(chat_history)
+    response = generate_response(user_input)
     
-    chat_history += response
+    history += tokenizer.bos_token + response + tokenizer.eos_token + " "
     print("Bot: " + response)
+
+    print(history)
